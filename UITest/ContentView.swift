b73abdfb5537c565f7cb290811c6efe83920b2d2
msahaydak <maksym.sahaydak@globallogic.com>
2020-09-11 17:10:55 +0300

    Initial Commit

---
+++ UITest/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  UITest
+//
+//  Created by Maksym Sahaydak on 11.09.2020.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ UITest/UITestApp.swift
@@ -0,0 +1,17 @@
+//
+//  UITestApp.swift
+//  UITest
+//
+//  Created by Maksym Sahaydak on 11.09.2020.
+//
+
+import SwiftUI
+
+@main
+struct UITestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
